91a4cd74da7f5f22bf14167da8dab893f4914000
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-04-14 14:36:16 +0200

    create winner score

---
+++ src/components/Score.js
@@ -0,0 +1,12 @@
+import "./Score.css"
+
+const Score = () => {
+  return (
+    <div className="score">
+      <span className="x_score"> X - 1</span>
+      <span className="o_score"> O - 1</span>
+    </div>
+  )
+}
+
+export default Score
