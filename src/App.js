61ca244d08d0d2ebcbb212565d9847ea3455e2ac
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-04-14 12:33:02 +0200

    add WIN_CONDITIONS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import Board from './components/Board';
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xTurnPlay, setXTurnPlay] = useState(true);
+  
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   const handleBoxClick = (squareID)=>{
     const updatedBoard = board.map((value, id) => {
